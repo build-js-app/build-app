7cf0395f8e70c02888a015388adfad7a2811c8d8
Yegor Sytnyk <yegor.sytnyk@gmail.com>
2017-03-07 12:36:21 +0800

    allow to run init command without specifying templates (using defaults)

--- lib/scripts/init.ts
+++ lib/scripts/init.ts
@@ -10,43 +10,9 @@ import pathHelper from '../helpers/pathHelper';
 import utils from '../helpers/utils';
 
 function init() {
-    let templateRegistry = fs.readJsonSync(pathHelper.moduleRelative('./assets/init/templates.json'));
-
     let args = process.argv.slice(2);
 
-    let message = 'Please, specify project templates as a first argument in a format {project_name}:{server_template}:{client_template}';
-
-    if (!args.length) {
-        return console.log(message);
-    }
-
-    let parts = args[0].split(':');
-
-    if (parts.length !== 3) {
-        return console.log(message);
-    }
-
-    let project = parts[0];
-    if (!templateRegistry.projects[project]) {
-        let projects = Object.keys(templateRegistry.projects);
-        return console.log(`Incorrect project name '${project}'. Valid values are: [${projects.join(', ')}].`);
-    }
-
-    let projectInfo = templateRegistry.projects[project];
-
-    let serverTemplate = parts[1];
-    let serverTemplateInfo = projectInfo.server[serverTemplate];
-    if (!serverTemplateInfo) {
-        let templates = Object.keys(projectInfo.server);
-        return console.log(`Incorrect server template '${serverTemplate}'. Valid values are: [${templates.join(', ')}].`);
-    }
-
-    let clientTemplate = parts[2];
-    let clientTemplateInfo = projectInfo.client[clientTemplate];
-    if (!clientTemplateInfo) {
-        let templates = Object.keys(projectInfo.client);
-        return console.log(`Incorrect client template '${clientTemplate}'. Valid values are: [${templates.join(', ')}].`);
-    }
+    let templatesInfo = getTemplatesInfo(args);
 
     let checkFolder = Promise.resolve(null);
     let root = pathHelper.projectRelative('./');
@@ -62,22 +28,73 @@ function init() {
             });
     }
 
-
     checkFolder
         .then(() => {
             return utils.logOperationAsync('Downloading server template',
-                downloadTemplate(serverTemplate, projectInfo.server[serverTemplate], pathHelper.serverRelative('./')));
+                downloadTemplate(templatesInfo.serverTemplate, pathHelper.serverRelative('./')));
         })
         .then(() => {
             return utils.logOperationAsync('Downloading client template',
-                downloadTemplate(clientTemplate, projectInfo.client[clientTemplate], pathHelper.clientRelative('./')));
+                downloadTemplate(templatesInfo.clientTemplate, pathHelper.clientRelative('./')));
         })
         .then(() => {
             copyAssets();
         })
 }
 
-function downloadTemplate(templateName, templateInfo, directory) {
+function getTemplatesInfo(args) {
+    let logAndExit = (message) => {
+        utils.log(message);
+        process.exit(0);
+    };
+
+    let templateRegistry = fs.readJsonSync(pathHelper.moduleRelative('./assets/init/templates.json'));
+
+    let message = 'Please, specify project templates as a first argument in a format {project_name}:{server_template}:{client_template}';
+
+    let params = [];
+
+    if (!args.length) {
+        params = templateRegistry.defaults;
+    } else {
+        params = args[0].split(':');
+    }
+
+    if (params.length !== 3) {
+        logAndExit(message);
+    }
+
+    let project = params[0];
+    if (!templateRegistry.projects[project]) {
+        let projects = Object.keys(templateRegistry.projects);
+        logAndExit(`Incorrect project name '${project}'. Valid values are: [${projects.join(', ')}].`);
+    }
+
+    let projectInfo = templateRegistry.projects[project];
+
+    let serverTemplate = params[1];
+    let serverTemplateInfo = projectInfo.server[serverTemplate];
+    serverTemplateInfo.name = serverTemplate;
+    if (!serverTemplateInfo) {
+        let templates = Object.keys(projectInfo.server);
+        logAndExit(`Incorrect server template '${serverTemplate}'. Valid values are: [${templates.join(', ')}].`);
+    }
+
+    let clientTemplate = params[2];
+    let clientTemplateInfo = projectInfo.client[clientTemplate];
+    clientTemplateInfo.name = clientTemplate;
+    if (!clientTemplateInfo) {
+        let templates = Object.keys(projectInfo.client);
+        logAndExit(`Incorrect client template '${clientTemplate}'. Valid values are: [${templates.join(', ')}].`);
+    }
+
+    return {
+        serverTemplate: serverTemplateInfo,
+        clientTemplate: clientTemplateInfo
+    }
+}
+
+function downloadTemplate(templateInfo, directory) {
     //TODO check not empty
     fs.emptyDirSync(directory);
 
