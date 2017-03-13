acf4452116ef6ab7396d8f978ede80838ab4c75a
Yegor Sytnyk <yegor.sytnyk@gmail.com>
2017-03-13 10:58:05 +0800

    install script for client/server

--- lib/scripts/install.ts
+++ lib/scripts/install.ts
@@ -18,9 +18,22 @@ function install() {
         process.exit(0);
     };
 
-
-
     let target = args[0];
+    let packageName = args[1];
+
+    if (!packageName) {
+        logAndExit(message);
+    }
+
+    if (target === 'server') {
+        return installPackage(packageName, target);
+    }
+
+    if (target === 'client') {
+        return installPackage(packageName, target);
+    }
+
+    logAndExit(message);
 }
 
 function installAll() {
@@ -40,6 +53,24 @@ function installAll() {
     utils.runCommand('npm', ['install'], {
         title: 'Install client dependencies',
         path: pathHelper.projectRelative(config.paths.client.root)
+    });
+}
+
+function installPackage(packageName, target) {
+    let command = 'npm';
+    let params = ['install', packageName, '--save', '--save-exact'];
+
+    if (utils.commandExists('yarn')) {
+        command = 'yarn';
+        params = ['add', packageName, '--no-lockfile'];
+    }
+
+    let folder = target === 'server' ? pathHelper.serverRelative('./')
+        : pathHelper.clientRelative('./');
+
+    utils.runCommand(command, params, {
+        title: `Install package '${packageName}' into ${target}`,
+        path: folder
     });
 }
 
